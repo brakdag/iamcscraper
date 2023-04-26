64eef8b273c84da26e50f69f32a28d750995865a
root <brakdag@gmail.com>
2023-04-26 11:54:56 -0300

    analizando pdf

--- test/iamcTest.js
+++ test/iamcTest.js
@@ -1,9 +1,10 @@
 const Iamc = require("../lib/iamc")
+
 describe("Testeando IAMC library",function(){
 
 it("IACM",function(done){
     let  a = new Iamc();
-    a.getUrlInform(2019,4,12,(x)=>{
+    a.getUrlInform(2023,4,25,(x)=>{
         console.log(x)
     });
     done()
